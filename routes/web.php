112a19719f3d464fcf8477494006b2f28b25bdf6
sophiaaa <sophiaaa@students.amikom.ac.id>
2025-05-15 23:15:49 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+
+// Route::get('/', function () {
+//     return Inertia::render('Welcome');
+// })->name('home');
+
+Route::get('/', [AuthenticatedSessionController::class, 'create'])
+        ->name('home');
+
+Route::get('dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
